d3067553215f8ee582325007f118b8bd9aee2717
MihailPetkov96 <mpetkov228@gmail.com>
2024-01-01 18:22:16 +0200

    Add classes for node and linked list

---
+++ linked-list.js
@@ -0,0 +1,12 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.nextNode = null;
+    }
+}
+
+class LinkedList {
+    constructor(head = null) {
+        this.head = head;
+    }
+}
