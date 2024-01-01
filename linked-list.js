b79c2dd4ae6e5673060b92e9959a11fcb49a77be
MihailPetkov96 <mpetkov228@gmail.com>
2024-01-01 18:30:58 +0200

    Implement append function for linked list

--- linked-list.js
+++ linked-list.js
@@ -9,4 +9,27 @@ class LinkedList {
     constructor(head = null) {
         this.head = head;
     }
+
+    append(value) {
+        if (this.head == null) {
+            return this.head = new Node(value);
+        }
+
+        let node = this.head;
+        while (node) {
+            if (!node.nextNode) {
+                node.nextNode = new Node(value);
+                break;
+            }
+
+            node = node.nextNode;
+        }
+    }
 }
+
+let list = new LinkedList();
+list.append(3);
+list.append(6);
+list.append(12);
+
+console.log(list);
